e9839876e16d50692ee805f5426b386c214a0211
Max Goldman <maxg@mit.edu>
2012-06-18 11:56:01 -0400

    Tutorial for micro-outsourcing

---
+++ micro/tutorial/src/Tutorial.java
@@ -0,0 +1,52 @@
+import org.junit.*;
+
+public class Tutorial {
+    
+    /* Welcome to Collabode, a web-based Java IDE */
+    
+    private boolean helloThere;
+    
+    /* Current Job */
+    
+    // The job you're working on is highlighted in the sidebar on the left
+    // If you have questions, you can chat with the job requester using the
+    //   "chat with requester" link
+    // You can try it now, although the practice job requester is probably
+    //   not online
+    
+    /* Editing and Running Code */
+    
+    // Edit the code so the main method below prints "Hello, world!"
+    // Now open the console: click the "Show Console" button in the toolbar
+    //   to open the console in a separate tab
+    // In that console tab, click the green run button (just opening the
+    //   console does not run the program)
+    
+    public static void main(String[] args) {
+        System.out.println();
+    }
+    
+    /* Running Tests */
+    
+    // The projects you work on might have JUnit tests (or they might not)
+    // To run the tests, click the "Run Tests" button in the toolbar
+    // When the tests are done, results will appear in the sidebar
+    
+    @Test public void testTesting() {
+        Assert.fail();
+    }
+    
+    /* More Features */
+    
+    // This editor has some of the same features as Eclipse:
+    // - Hover over error and warning markers in the margin to see details
+    // - Use ctrl-space to use code completion (or pause after typing '.')
+    // - Use ctrl-shift-o to use organize imports
+    
+    /* Finish a Job */
+    
+    // When you finish a job, or have worked on it for about 15 minutes,
+    //   you should click "work complete" in the top right corner
+    // When you're done with this tutorial, click "work complete" :)
+    
+}
