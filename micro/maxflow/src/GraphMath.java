b08df247f3783b3ffe7c69d7dca78de69ebd2ed9
Max Goldman <maxg@mit.edu>
2012-06-26 13:02:17 -0400

    Remove garbage character

--- micro/maxflow/src/GraphMath.java
+++ micro/maxflow/src/GraphMath.java
@@ -1,7 +1,7 @@
 public class GraphMath {
     
     public static int maxFlow(Graph graph, String source, String sink) {
-        // TODO implement https://en.wikipedia.org/wiki/Ford–Fulkerson_algorithm
+        // TODO implement https://en.wikipedia.org/wiki/Ford-Fulkerson_algorithm
         return 0;
     }
     
