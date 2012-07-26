8c50d89f24134722e3199d0e19fc968cd90a4982
Max Goldman <maxg@mit.edu>
2012-07-26 15:30:20 -0400

    Task: bus fare state machine

---
+++ tdpp/busfare/src/FareMachineTest.java
@@ -0,0 +1,16 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import collabode.Accepted;
+import collabode.NeedsImpl;
+import collabode.NeedsReview;
+
+/**
+ * JUnit: {@link Test}, {@link Assert}
+ * Annotations: none -> {@link NeedsImpl} -> {@link NeedsReview} -> {@link Accepted}
+ */
+public class FareMachineTest {
+    
+    
+    
+}
